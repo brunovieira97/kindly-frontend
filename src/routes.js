96b9b551f0e9569cff6b19665505bba75dfb761e
Orlando Hidalgo <ohidalgo@gvdasa.com.br>
2019-10-28 08:34:50 -0300

    Ajuste nas rotas para não exibir menu no login

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
+import TopMenu from './components/TopMenu'
 import SignUp from './pages/SignUp'
 import SignUpDetails from './pages/SignUpDetails'
 import SignIn from './pages/SignIn'
 
 
-const Routes = () => (
-      <Switch>
-        <Route exact path="/" component={() => <h1>HELLO THERE!</h1>} />
-        <Route exact path="/signup" component={() => <SignUp />} />
-        <Route exact path="/signin" component={() => <SignIn />} />
-        <Route exact path="/signUpDetails" component={() => <SignUpDetails />} />
-        <Route path="*" component={() => <h1>Page not found</h1>} />
-      </Switch>
-  );
-  
-  export default Routes;
+const Routes = withRouter(({ location }) => (
+  <div>
+    {(location.pathname !== '/signup' && location.pathname !== '/signin' && location.pathname !== '/signUpDetails') && <TopMenu />}
+    <Switch>
+      <Route exact path="/" component={() => <h1>HELLO THERE!</h1>} />
+      <Route exact path="/signup" component={() => <SignUp />} />
+      <Route exact path="/signin" component={() => <SignIn />} />
+      <Route exact path="/signUpDetails" component={() => <SignUpDetails />} />
+      <Route path="*" component={() => <h1>Page not found</h1>} />
+    </Switch>
+  </div>
+));
+
+export default Routes;
